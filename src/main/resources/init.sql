5e47e03da2885f0fc0e1e5404cd62854bba3e940
HauNT1409021997 <akitosama12546@gmail.com>
2024-11-30 21:57:00 +0700

    update backend

--- src/main/resources/init.sql
+++
@@ -1,5 +0,0 @@
--- Insert initial data into the 'users' table
-INSERT INTO test.user_info (id, name, email)
-VALUES (0, 'alice', 'alice@example.com'),
-       (1, 'bob', 'bob@example.com')
-ON CONFLICT DO NOTHING;
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,14 +1,21 @@
 -- Create the 'test' schema if it doesn't exist
-CREATE SCHEMA IF NOT EXISTS test;
+CREATE SCHEMA IF NOT EXISTS postgresqlDB;
 
--- Switch to the 'test' schema
-SET search_path TO test;
+-- Switch to the 'postgresqlDB' schema
+SET search_path TO postgresqlDB;
 
-DROP TABLE IF EXISTS test.test.user_info;
+DROP TABLE IF EXISTS postgresqlDB.user_info;
 -- Create the 'users' table if it doesn't exist
-CREATE TABLE IF NOT EXISTS user_info
+CREATE TABLE IF NOT EXISTS postgresqlDB.user_info
 (
     id    SERIAL  PRIMARY KEY,
     name  VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL
 );
+
+-- Insert initial data into the 'users' table
+-- INSERT INTO postgresqlDB.user_info (id, name, email)
+-- VALUES (0, 'alice', 'alice@example.com'),
+--        (1, 'bob', 'bob@example.com')
+-- ON CONFLICT DO NOTHING;
+
